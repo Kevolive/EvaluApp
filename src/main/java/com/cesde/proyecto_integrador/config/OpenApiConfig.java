7fae9e9ccbbd5cc9d1bdd570a003e58e110640c4
Niko20000 <nikogoku.13@gmail.com>
2025-05-17 14:13:04 -0500

    Se agregan otros atributos al OpenConfig, en la ruta src/config/data/OpenApiConfig, se le agrega el contact

--- src/main/java/com/cesde/proyecto_integrador/config/OpenApiConfig.java
+++ src/main/java/com/cesde/proyecto_integrador/config/OpenApiConfig.java
@@ -1,6 +1,7 @@
 package com.cesde.proyecto_integrador.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
@@ -13,10 +14,15 @@ public class OpenApiConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
+                .version("beta")
                 .title("API - Proyecto Integrador - Backend II")  // Custom title                        
-                .description("CESDE - Desarrollo de Software"))
+                .description("CESDE - Desarrollo de Software")
+                .contact(new Contact()
+                        .name("CESDE")
+                        .email("kevinolivella@gmail.com")))
                 .addServersItem(new Server()
                 .url("http://localhost:8080")
-                .description("Servidor Local"));                
+                .description("Servidor Local")
+                );                
     }
 }
