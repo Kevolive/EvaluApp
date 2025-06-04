fd44a922a6d484151349ef05fe6e7c3591813277
Kevin Olivella <kevinolivella@gmail.com>
2025-06-03 22:35:49 -0500

    se elemina el Map

--- src/main/java/com/cesde/proyecto_integrador/repository/OpcionRepository.java
+++ src/main/java/com/cesde/proyecto_integrador/repository/OpcionRepository.java
@@ -1,6 +1,6 @@
 package com.cesde.proyecto_integrador.repository;
 import java.util.List;
-import java.util.Map;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
