bcc77e86bf8508fd8ac2e3eddffe2a77e09ee66f
Kevin <kevinolivella@gmail.com>
2025-06-07 15:53:09 -0500

    Se modifica la carpeta CorsConfig.java para que permita el origen

--- src/main/java/com/cesde/proyecto_integrador/controller/ExamenController.java
+++ src/main/java/com/cesde/proyecto_integrador/controller/ExamenController.java
@@ -33,6 +33,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/examenes")
+@CrossOrigin(origins = "http://localhost:5173")
 @Tag(name = "Examenes", description = "API para gestionar examenes")
 public class ExamenController {
 
